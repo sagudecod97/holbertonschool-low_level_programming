df3a369ae804cc169afb4925915b99b1e535ccfb
Santiago <sagudecod97@gmail.com>
2019-06-20 22:26:18 +0000

    Created files from 5 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/*
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int n;
+
+n = 0;
+
+while (n < 10)
+{
+printf("%d", n);
+n++;
+};
+putchar('\n');
+return (0);
+
+}
---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/*
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int n;
+
+n = 122;
+
+while (n > 96)
+{
+putchar(n);
+n--;
+};
+putchar('\n');
+return (0);
+
+}
