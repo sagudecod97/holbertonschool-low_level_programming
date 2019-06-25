3fe52fd9287544ab996a155e5cd1b127f00e23ee
Santiago <sagudecod97@gmail.com>
2019-06-25 23:25:53 +0000

    Fixed all betty issues to 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,18 +1,23 @@
 #include "holberton.h"
+/**
+ * print_sign - prints the sign of the number
+ * @n: Number to test
+ * Return: 1, 0 or -1
+ */
 
-int print_sign(int n){
+int print_sign(int n)
+{
 
 	if (n > 0)
 	{
 		_putchar ('+');
 		return (1);
-	}else if (n == 0)
+	} else if (n == 0)
 	{
 		_putchar ('0');
 		return (0);
-	}else
-	{
-		_putchar ('-');
-		return (-1);
 	}
+	_putchar ('-');
+	return (-1);
+
 }
