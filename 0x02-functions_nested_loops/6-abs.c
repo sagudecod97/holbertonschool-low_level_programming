be6ab41d0a88b1c22c2a2459f2b1f279bfdcbb7b
Santiago <sagudecod97@gmail.com>
2019-06-25 23:31:29 +0000

    Fixed all betty issues of 6-abs.c file

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,14 +1,20 @@
 #include "holberton.h"
 
-int _abs(int n){
+/**
+ * _abs - Returns the value always positive
+ * @n: Number to treat
+ * Return: n
+ */
+
+int _abs(int n)
+{
 
 	if (n < 0)
 	{
 		int abs;
+
 		abs = n * -1;
 		return (abs);
-	}else
-	{
-		return (n);
 	}
+	return (n);
 }
