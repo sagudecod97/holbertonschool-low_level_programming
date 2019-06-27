9ad4b04919d4dcca40b9e80a4bc3e5df20a44b4e
Santiago <sagudecod97@gmail.com>
2019-06-27 23:32:16 +0000

    Modified 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,16 +13,15 @@ int main(void)
 	{
 		if ((n % 3) == 0 && (n % 5) == 0)
 		{
-			printf("FizzBuzz");
+			printf("FizzBuzz ");
 		} else if ((n  % 3) == 0)
 		{
-			printf("Fizz");
+			printf("Fizz ");
 		} else if ((n % 5) == 0)
 		{
-			printf("Buzz");
+			printf("Buzz ");
 		} else
-			printf("%d", n);
-			putchar (32);
+			printf("%d ", n);
 	}
 		putchar ('\n');
 		return (0);
