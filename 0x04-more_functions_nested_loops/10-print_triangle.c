cf67ab26f160222da3c8bac335605dee28791840
Santiago <sagudecod97@gmail.com>
2019-06-28 13:38:30 +0000

    Created file 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+
+/**
+ * print_triangle - Prints triangules
+ * @size: Size of the triangule
+ * Return: The triangule
+ */
+
+void print_triangle(int size)
+{
+	int n, space, square;
+
+	for (n = 1; n <=  size; n++)
+	{
+		if (size == 0)
+		{
+			_putchar ('\n');
+		};
+
+		space = size;
+
+		while (space > n)
+		{
+			_putchar ('.');
+			space--;
+		}
+
+		square = space - n;
+
+		while (square < space)
+		{
+			_putchar (35);
+			square++;
+		}
+		_putchar('\n');
+	}
+}
