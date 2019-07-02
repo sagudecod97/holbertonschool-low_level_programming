fbc3f1a322fef79ce4f0f7ca42d08b6c1e59555b
Santiago <sagudecod97@gmail.com>
2019-07-02 16:46:23 +0000

    Modified Holberton.h

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,4 +1,4 @@
-fndef _HOLBERTON_H_
+#ifndef _HOLBERTON_H_
 #define _HOLBERTON_H_
 
 /*
@@ -6,7 +6,7 @@ fndef _HOLBERTON_H_
  * Typedefs
  * Function prototypes
  */
-
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
