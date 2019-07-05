9107fa07629f35d1542730bbc3e1fdc8edbf312c
Santiago <sagudecod97@gmail.com>
2019-07-05 02:18:15 +0000

    Created file 7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * *leet - Encodes a string into 1337
+ * @s: String to encode.
+ * Return: String encoded.
+ */
+
+char *leet(char *s)
+{
+	char alpha[] = "AaEeOoTtLl";
+
+	char num[] = "4433007711";
+
+	int i = 0, j;
+
+	while (s[i] != '\0')
+	{
+		for (j = 0; j < 9; j++)
+		{
+			if (s[i] == alpha[j])
+			{
+				s[i] = num[j];
+			}
+		}
+		i++;
+	}
+	return (s);
+}
