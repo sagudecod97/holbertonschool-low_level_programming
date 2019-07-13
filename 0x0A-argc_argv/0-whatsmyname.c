961860e53b1582206e6456390c22fb6a1b9925ad
Santiago <sagudecod97@gmail.com>
2019-07-13 19:39:02 +0000

    Fixed issues related with betty to file 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main - Will print the name of the executable
+ * @argc: Size of the argv array
+ * @argv: Array of arguments
+ * Return: 0 on Success
+ */
+
 int main(int argc, char *argv[])
 {
-	printf ("%s\n", argv[argc - argc]);
+	printf("%s\n", argv[argc - argc]);
 
 	return (0);
 }
