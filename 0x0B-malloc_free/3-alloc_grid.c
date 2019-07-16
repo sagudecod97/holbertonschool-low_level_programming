3c633dafb8374713bd8e70426c0cf8cd9b2cffa3
Santiago <sagudecod97@gmail.com>
2019-07-16 20:02:08 +0000

    Created files 2-str_concat.c and 3-alloc_grid.c

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * **alloc_grid - Function returns a pointer to a 2 dimensional array of integers
+ * @width: Width of Array
+ * @height: Heigth of Array
+ * Return: Pointer
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **pint = (int **)malloc(height * sizeof(int));
+
+	int i, j, h, zero = 0;
+
+	if ((width <= 0) | (height <= 0) | (pint == NULL))
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		pint[i] = (int *)malloc(width * sizeof(int));
+	}
+	
+	for (h = 0; h < height; h++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			pint[h][j] = zero;
+		}
+	}
+
+	return (pint);
+}
