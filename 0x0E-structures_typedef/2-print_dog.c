ba738efcfa982965420c79ba39006380569a503a
Santiago <sagudecod97@gmail.com>
2019-07-22 17:20:31 +0000

    Created header dog.h, and also files 1 and 2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,39 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_dog - Function that prints a struct dog
+ * @d: Pointer
+ * Return: Nothing
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+
+	if ((*d).name != NULL)
+	{
+		printf("Name: %s\n", (*d).name);
+	} else
+	{
+		printf("Name: (nil)\n");
+	}
+	if ((*d).age != 0)
+	{
+		printf("Age: %f\n", (*d).age);
+	} else
+	{
+		printf("Age: (nil)\n");
+	}
+	if ((*d).owner != NULL)
+	{
+		printf("Owner: %s\n", (*d).owner);
+	} else 
+	{
+		printf("Owner: (nil)\n");
+	}
+}
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,32 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+
+/**
+ * struct dog - Structure of a dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Description: Structure of the simplest data from any kind of dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * struc dog - Typedef for dog struct
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+#endif
