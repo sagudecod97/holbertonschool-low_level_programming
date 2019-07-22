c0c6107bb4b192975d3ee70e6b805086904abed1
Santiago <sagudecod97@gmail.com>
2019-07-22 20:42:02 +0000

    Added prototype to header and created file 5

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,4 +1,5 @@
 #ifndef _DOG_H_
+
 #define _DOG_H_
 
 /**
@@ -24,4 +25,6 @@ typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
 #endif
