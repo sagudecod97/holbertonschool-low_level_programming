fba11a7dec05e8f740a60b78b22d2b3a4152a778
Santiago <sagudecod97@gmail.com>
2019-07-24 18:36:47 +0000

    Modified if statment to match any case

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -14,7 +14,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
-	if (array == NULL)
+	if (array == NULL || size == NULL || action == NULL)
 	{
 		return;
 	};
