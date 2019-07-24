f84f93a0b6f21b25317255fd4741b85ad53359b7
Santiago <sagudecod97@gmail.com>
2019-07-24 23:18:27 +0000

    Fixed comparison with character %

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -30,8 +30,8 @@ int main(int argc, char *argv[])
 		exit(99);
 	};
 	if ((strcmp(argv[2], "/") == 0
-		|| strcmp(argv[2], "%"))
-		&& strcmp(argv[3], "0") == 0)
+		|| strcmp(argv[2], "%") == 0)
+		&& atoi(argv[3]) == 0)
 	{
 		printf("Error\n");
 		exit(100);
