ca6348076d5e3f3b047c326203f0b0e09fd4436e
Santiago <sagudecod97@gmail.com>
2019-08-06 19:52:13 +0000

    Modified the positon of malloc, and also fixed in case of idx = 0

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -13,11 +13,17 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int counter = 0, i;
 	listint_t *tmp, *tmp2, *node;
 
-	if (*head == NULL || head == NULL)
+	node = (listint_t *)malloc(sizeof(listint_t));
+	if (node == NULL)
 		return (NULL);
-
+	if (idx == 0)
+	{
+		node->n = n;
+		node->next = *head;
+		*head = node;
+		return (node);
+	}
 	tmp = *head;
-
 	while (tmp != NULL)
 	{
 		counter++;
@@ -25,14 +31,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	};
 	if (idx < counter)
 	{
-		node = (listint_t *)malloc(sizeof(listint_t));
-
-		if (node == NULL)
-			return (NULL);
-
 		tmp2 = (*head)->next;
 		tmp = *head;
-
 		for (i = 1; i <= idx; i++)
 		{
 			if (i == idx)
@@ -46,6 +46,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			tmp = tmp->next;
 		}
 	}
-
 	return (NULL);
 }
