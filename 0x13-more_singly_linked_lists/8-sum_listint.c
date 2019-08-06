202d0fc2dc822f950bb9d430fae04471c85ab917
Santiago <sagudecod97@gmail.com>
2019-08-06 18:46:48 +0000

    Created file 8 with Betty OK

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * sum_listint - Function that returns the sum of all the data (n)
+ * @head: Head
+ * Return: Sum of all data
+ */
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	listint_t *temp;
+
+	if (head == NULL)
+		return (0);
+
+	temp = head->next;
+
+	while (temp != NULL)
+	{
+		sum += temp->n;
+		temp = temp->next;
+	};
+
+	return (sum);
+}
