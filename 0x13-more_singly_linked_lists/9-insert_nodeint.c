e65eb9c2c7bfc2ae045f7d08fdeec8c9c7ebfc5f
Santiago <sagudecod97@gmail.com>
2019-08-06 19:35:21 +0000

    Created file 9 with Betty OK

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,51 @@
+#include "lists.h"
+
+/**
+ * *insert_nodeint_at_index - Inserts a new node at a given position
+ * @head: Head
+ * @idx: Index
+ * @n: Integer
+ * Return: Addres
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int counter = 0, i;
+	listint_t *tmp, *tmp2, *node;
+
+	if (*head == NULL || head == NULL)
+		return (NULL);
+
+	tmp = *head;
+
+	while (tmp != NULL)
+	{
+		counter++;
+		tmp = tmp->next;
+	};
+	if (idx < counter)
+	{
+		node = (listint_t *)malloc(sizeof(listint_t));
+
+		if (node == NULL)
+			return (NULL);
+
+		tmp2 = (*head)->next;
+		tmp = *head;
+
+		for (i = 1; i <= idx; i++)
+		{
+			if (i == idx)
+			{
+				node->n = n;
+				node->next = tmp2;
+				tmp->next = node;
+				return (*head);
+			};
+			tmp2 = tmp2->next;
+			tmp = tmp->next;
+		}
+	}
+
+	return (NULL);
+}
