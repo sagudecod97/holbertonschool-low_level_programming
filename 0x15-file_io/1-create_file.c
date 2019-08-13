7261e49275dd8171debacee98d5bd981578b87c3
Santiago <sagudecod97@gmail.com>
2019-08-13 20:27:17 +0000

    Erased malloc, and change position of open()

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,37 +9,32 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int i = 0, j = 0, fd = 0, check = 0;
-	char *buffer;
+	int i = 0, fd = 0, check = 0;
 
 	if (filename == NULL)
 		return (-1);
-	if (text_content == NULL)
-		text_content = "";
-	while (text_content[i])
-		i++;
-
-	buffer = malloc(sizeof(char *) * i);
-
-	if (buffer == NULL)
-		return (-1);
-	while (text_content[j])
-	{
-		buffer[j] = filename[j];
-		j++;
-	};
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (text_content == NULL)
+	{
+		return (1);
+		close(fd);
+	};
+
+	while (text_content[i])
+		i++;
 
 	if (fd == -1)
 		return (-1);
 
-	check = write(fd, buffer, i);
+	check = write(fd, text_content, i);
 
 	if (check == -1)
+	{
+		write(STDOUT_FILENO, "fails", 5);
 		return (-1);
+	};
 
-	free(buffer);
-	close(fd);
 	return (1);
 }
