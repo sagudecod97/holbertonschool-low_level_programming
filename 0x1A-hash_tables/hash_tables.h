824070ce548625a8758b4fa0c99ae99a11bd14a4
Santiago <sagudecod97@gmail.com>
2019-10-11 21:09:07 +0000

    Created djb2 function and added its protype to the header

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -12,6 +12,7 @@
  * @value: The value corresponding to a key
  * @next: A pointer to the next node of the List
  */
+
 typedef struct hash_node_s
 {
 	char *key;
@@ -29,6 +30,7 @@ typedef struct hash_node_s
  * because we want our HashTable to use a Chaining
  * collision handling
  */
+
 typedef struct hash_table_s
 {
 	unsigned long int size;
@@ -36,6 +38,7 @@ typedef struct hash_table_s
 } hash_table_t;
 
 /** Prototypes **/
+
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
@@ -43,5 +46,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
+unsigned long int hash_djb2(const unsigned char *str);
 
 #endif
