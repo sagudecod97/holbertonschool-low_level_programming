6721048a1ea70342425d61c7d93144fe436c75cb
Santiago <sagudecod97@gmail.com>
2019-10-11 22:56:59 +0000

    Fixed Betty issues

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -4,6 +4,7 @@
  * hash_table_get - Retrieves a value associated with a key
  * @ht: Hash table
  * @key: Key to retrieve
+ * Return: Value of key
  */
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
