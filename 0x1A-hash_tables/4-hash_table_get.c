b6f50c283e77c90d977888a93538f82cf4b90a49
Santiago <sagudecod97@gmail.com>
2019-10-11 22:55:04 +0000

    Created file 4-hash_table_get.C

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - Retrieves a value associated with a key
+ * @ht: Hash table
+ * @key: Key to retrieve
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index = 0;
+	hash_node_t *tmp;
+
+	if (ht == NULL || key == NULL)
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	tmp = ht->array[index];
+
+	while (tmp != NULL)
+	{
+		if (strcmp(tmp->key, key) == 0)
+			return (tmp->value);
+		tmp = tmp->next;
+	}
+	return (NULL);
+}
