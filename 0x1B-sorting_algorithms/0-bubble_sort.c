74090a5da59df0a74da238a7d0212507a096bec1
Santiago <sagudecod97@gmail.com>
2019-10-28 02:29:50 +0000

    Created files 0-bubble, 1-insertion, 2-selection, and header

---
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - bubble sort algorithm
+ * @array: array
+ * @size: size of the arry
+ * Return: Nothing
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	int holder = 0;
+	unsigned int i;
+
+	for (i = 0; array[i] ; i++)
+	{
+		if (i == size - 1)
+			break;
+
+		else if (array[i] > array[i + 1])
+		{
+			holder = array[i];
+			array[i] = array[i + 1];
+			array[i + 1] = holder;
+			print_array(array, size);
+			i = -1;
+		}
+	}
+}
---
+++ 0x1B-sorting_algorithms/1-insertion_sort_list.c
@@ -0,0 +1,40 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - Insertion sort algorithm
+ * @list: List to sort
+ * Return: Nothing
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *temporal = NULL, *head = NULL;
+
+	temporal = *list;
+	head = *list;
+
+
+	temporal = temporal->next;
+
+
+	for (; temporal; temporal = temporal->next)
+	{
+		while ((temporal->prev) && temporal->prev->n > temporal->n)
+		{
+			temporal->prev->next = temporal->next;
+			if (temporal->next)
+				temporal->next->prev = temporal->prev;
+			temporal->next = temporal->prev;
+			temporal->prev = temporal->prev->prev;
+			temporal->next->prev = temporal;
+
+			if (temporal->prev == NULL)
+				*list = temporal;
+			else
+				temporal->prev->next = temporal;
+
+			print_list(*list);
+		}
+	}
+}
+
