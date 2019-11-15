65b5066aecd381f129fed98a3245e1811a1e8dde
Santiago <sagudecod97@gmail.com>
2019-11-15 20:18:45 +0000

    Fixed the syntax issue at the first if

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 def island_perimeter(grid):
-    if len(grid) = 0:
+    if len(grid) == 0:
         return 0
 
     zeros = 0
     ones = 0
     perimeter = 0
-    cells = len(grid) * len(grid)
+    cells = 0
 
     for i in range(len(grid)):
         for j in range(len(grid[i])):
+            cells += 1
             if grid[i][j] == 1:
                 ones += 1
             else:
