57564c981b173d2e1c663f72337c40032c9949ff
Santiago <sagudecod97@gmail.com>
2019-12-05 17:53:38 +0000

    Created file 17

---
+++ 0x1D-binary_trees/17-binary_tree_sibling.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+ * *binary_tree_sibling - Finds the sibling of a node
+ * @node: Node to check
+ * Return: Node's sibling
+ */
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (node->parent == NULL)
+		return (NULL);
+
+	if (node->parent->left && node->parent->right)
+	{
+		if (node->parent->left == node)
+			return (node->parent->right);
+		else
+			return (node->parent->left);
+	};
+	return (NULL);
+}
